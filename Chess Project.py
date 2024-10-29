2f6cbb81165b7afcd02d8a504c189464b9d33a83
Winwin <winpiny@gmail.com>
2024-10-29 17:08:39 +0700

    Add files via upload

---
+++ Chess Project.py
@@ -0,0 +1,19 @@
+import requests
+#requests package can be used to get API
+
+#Grabs PGN (game notation) from lichess API
+#api_url = 'https://lichess.org/game/export/[game ID]?action=pgnInJson'
+#Add ?action=[action]... to specify request
+api_url = 'https://lichess.org/game/export/28BX1zzd?action=pgnInJson'
+response = requests.get(api_url)
+
+# If the responses status code is 200 (success)
+if response.status_code == 200:
+    #data = response.json()
+    print(response.text)
+    #prints the game notation
+
+#Grabs evaluation of a given FEN (position) from chessdb API
+
+#api_url = 'http://www.chessdb.cn/cdb.php?action=[ACTION]{&[OPTION1]=[VALUE1]...&[OPTIONn]=[VALUEn]}'
+#api_url = 'http://www.chessdb.cn/cdb.php?action=queryall{&board=rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1}'
