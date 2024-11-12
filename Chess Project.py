427953943fd6d4018846a14e412f2fb7d91ac963
Winwin <winpiny@gmail.com>
2024-11-12 03:36:51 +0000

    Update Chess Project.py

--- Chess Project.py
+++ Chess Project.py
@@ -1,19 +1,36 @@
 import requests
-#requests package can be used to get API
 
-#Grabs PGN (game notation) from lichess API
-#api_url = 'https://lichess.org/game/export/[game ID]?action=pgnInJson'
-#Add ?action=[action]... to specify request, & to include multiple requests
-api_url = 'https://lichess.org/game/export/28BX1zzd?action=pgnInJson&tags=false&clocks=false&evals=false'
-response = requests.get(api_url)
+def lichess():
+    game_code = input("Enter the game code: ")
+    api_url = 'https://lichess.org/game/export/' + game_code + '?action&tags=false&clocks=false&evals=false&division=false'
+    response = requests.get(api_url, headers={"Accept": "application/json"})
 
-# If the responses status code is 200 (success)
-if response.status_code == 200:
-    #data = response.json()
-    print(response.text)
-    #prints the game notation
+    if response.status_code == 200:
+        data = response.json()
+        #print(data)
 
-#Grabs evaluation of a given FEN (position) from chessdb API
+    # Prints opening name
+    print(data['opening']['name'])
 
-#api_url = 'http://www.chessdb.cn/cdb.php?action=[ACTION]{&[OPTION1]=[VALUE1]...&[OPTIONn]=[VALUEn]}'
-#api_url = 'http://www.chessdb.cn/cdb.php?action=queryall{&board=rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1}'
+    # Prints a list of each move, good for future iteration
+    move_list = data['moves'].split()
+    print(move_list)
+
+def chessdb(FEN):
+    api_url = 'http://www.chessdb.cn/cdb.php?action=queryall&board=' + FEN + '&json=true'
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        data = response.json()
+    else:
+        print('API error')
+
+    if 'moves' in data:
+            moves = data['moves']
+            print('Suggested Moves and Evaluations:')
+            for move in moves:
+                print(f"Move: {move.get('san')}, Centipawn evaluation: {move.get('score')},\
+Expected score: {round(float(move.get('winrate'))*0.01,4)}")
+                
+lichess()
+chessdb('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
