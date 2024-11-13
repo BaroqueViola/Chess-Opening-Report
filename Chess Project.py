71b5e946051bc31af9423271f72a5882efd40a47
Winwin <winpiny@gmail.com>
2024-11-13 08:00:28 +0000

    Update Chess Project.py

--- Chess Project.py
+++ Chess Project.py
@@ -38,6 +38,32 @@ def qall(FEN):
             for move in moves:
                 print(f"Move: {move.get('san')}, {move.get('score')}, \
 {round(float(move.get('winrate'))*0.01,4)}")
+
+def qbest(FEN):
+    api_url = 'http://www.chessdb.cn/cdb.php?action=querybest&board=' + FEN + '&json=true'
+    response = requests.get(api_url)
+    
+    # If API request is successful
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+    else:
+        print('API error')
+        return 0
+
+def qscore(FEN):
+    api_url = 'http://www.chessdb.cn/cdb.php?action=queryscore&board=' + FEN + '&json=true'
+    response = requests.get(api_url)
+    
+    # If API request is successful
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+    else:
+        print('API error')
+        return 0
                 
-lichess()
-qall('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
+#lichess()
+#qall('r1bqkb1r/1ppp1ppp/p1n5/4p3/B2Pn3/5N2/PPP2PPP/RNBQ1RK1 b kq - 0 6')
+qbest('r1bqkb1r/1ppp1ppp/p1n5/4p3/B2Pn3/5N2/PPP2PPP/RNBQ1RK1 b kq - 0 6')
+qscore('r1bqkb1r/1ppp1ppp/p1n5/4p3/B2Pn3/5N2/PPP2PPP/RNBQ1RK1 b kq - 0 6')
