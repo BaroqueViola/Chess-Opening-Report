5811b9a4818e606c79df2414c21558ef8d5be552
Winwin <winpiny@gmail.com>
2024-11-14 09:20:32 +0000

    Update Evaluation conversion.py

--- Code/Evaluation conversion.py
+++ Code/Evaluation conversion.py
@@ -1,7 +1,10 @@
 import math
 
-def evalconversion(centipawn):
-    
+# Lichess formula
+def score(centipawn):
+    return round((50 + 50 * (2 / (1 + math.exp(-0.00368208 * centipawn)) - 1)) / 100, 4)
+
+# Stockfish formula (WIP)
 def win_rate(x):
     return 1 / (1 + math.exp(-(x-a)/b))
 def loss_rate(x):
